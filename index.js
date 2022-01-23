f92bdb2807c27e0b991b2396488acdbd1c021969
alex22-11 <98189149+alex22-11@users.noreply.github.com>
2022-01-23 23:05:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let count = 0
 
 let countEl = document.getElementById("count-el")
+let errorParagraph = document.getElementById("error")
 
 function increment() {
     count = count + 1
     countEl.innerText = count
 }
 
+function error(){
+    errorParagraph.textContent = "Something went wrong, please try again"
+}
